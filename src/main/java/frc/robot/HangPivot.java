51c9b0dfe64959947f5698e4c5b492fd6f27abaa
Wincy Perez <wincyperez@gmail.com>
2022-01-24 18:17:36 -1000

    Readjusted methods for pivoting forward and backward. Added the use of limit switches and added more to the "run" display.

--- src/main/java/frc/robot/HangPivot.java
+++ src/main/java/frc/robot/HangPivot.java
@@ -1,27 +1,37 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.kauailabs.navx.frc.AHRS;
 
 public class HangPivot {
     
-    //MOTOR (MINI CIM)
+    //MOTOR (775) & VERSA-PLANETARY ENCODER
     private MotorController hangPivot;
-    
+    private Encoder pivotEncoder;       //MAY NOT BE THE RIGHT ENCODER CLASS
+
+    //LIMIT SWITCHES
+    private DigitalInput frontSwitch;
+    private DigitalInput backSwitch;
+
     //GYRO (NAVX)
     private AHRS navX;
 
     //VARIABLES [SUBJECT TO CHANGE]
-    private final float tiltAngleForward = 0;
-    private final float tiltAngleBackward = 0;
-    private final float tiltAngleHome = 0;
+    private final double forwardPivotPos = 0.0;
+    private final double backwardPivotPos = 0.0;
+    private final double homePos = 0.0;
     
-    private final double forwardSpeed = 0.0;
-    private final double backwardSpeed = 0.0;
+    private final double forwardPivotSpeed = 0.0;
+    private final double backwardPivotSpeed = 0.0;
     
-    public HangPivot (MotorController pivotMotor, AHRS gyro){   //CONSTRUCTOR
+    public HangPivot (MotorController pivotMotor, Encoder hangPivotEncoder, AHRS gyro, DigitalInput frontLimitSwitch, DigitalInput backLimitSwitch){   //CONSTRUCTOR
         hangPivot = pivotMotor;
+        pivotEncoder = hangPivotEncoder;
+        frontSwitch = frontLimitSwitch;
+        backSwitch = backLimitSwitch;
         navX = gyro;
     }
 
@@ -44,37 +54,58 @@ public class HangPivot {
         pivotState = States.HOME;
     }
 
+    //CHECKS
+    private boolean backLimitTouched(){
+        return backSwitch.get();
+    }
+
+    private boolean frontLimitTouched(){
+        return frontSwitch.get();
+    }
+
     //METHODS
     private void pivotForward(){
-        if(navX.getPitch() < tiltAngleForward){
-            hangPivot.set(forwardSpeed);
+        if(!backLimitTouched()){
+            if(pivotEncoder.get() < forwardPivotPos){
+                hangPivot.set(forwardPivotSpeed);
+            }
         }
-        
-        else {
+
+        else{
             hangPivot.set(0);
         }
     }
 
     private void pivotBackward(){
-        if(navX.getPitch() < tiltAngleBackward){
-            hangPivot.set(backwardSpeed);
+        if(!frontLimitTouched()){
+            if(pivotEncoder.get() > backwardPivotPos){
+                hangPivot.set(backwardPivotSpeed);
+            }
         }
 
-        else {
+        else{
             hangPivot.set(0);
         }
     }
 
-    private void nothing(){ //METHOD FOR TESTING CODE
+    private void pivotHome(){
+        
+    }
+
+    private void testing(){ //METHOD FOR TESTING CODE
 
     }
 
     public void run(){
+        SmartDashboard.putString("HANG PIVOT STATE", pivotState.toString());
+        SmartDashboard.putBoolean("BACK LIMIT", backSwitch.get());
+        SmartDashboard.putBoolean("FRONT LIMIT", frontSwitch.get());
+        SmartDashboard.putNumber("PIVOT ENCODER", pivotEncoder.get());
         
         switch(pivotState){
             
             case TESTING:
-            nothing();
+            testing();
             break;
 
             case PIVOTFORWARD:
